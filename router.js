2be674123f8d7571d169826bb2f8e50e863446db
Çağdaş <cagdas.oztekin@ug.bilkent.edu.tr>
2016-08-22 22:52:54 +0300

    Add console message

--- router.js
+++ router.js
@@ -107,6 +107,8 @@ module.exports = (function() {
             req.socket.remoteAddress ||
             req.connection.socket.remoteAddress;
 
+        console.log('Hola');
+
         var classroom = req.body.classroom;
         var hours = req.body.hours;
 
